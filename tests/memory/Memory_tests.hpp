e84615f6d93512ed55c9095d74a6caf7b200d9a4
Avereniect <Avereniect@gmail.com>
2020-06-24 13:34:26 -0700

    Updated tests

--- tests/memory/Memory_tests.hpp
+++ tests/memory/Memory_tests.hpp
@@ -1,9 +1,9 @@
 #ifndef AUL_MEMORY_TESTS_HPP
 #define AUL_MEMORY_TESTS_HPP
 
-#include "../memory/Memory_tests.hpp"
+#include <aul/memory/Memory.hpp>
 
-#include <catch2/catch.hpp>
+#include <gtest/gtest.h>
 
 #include <algorithm>
 #include <memory>
@@ -11,50 +11,15 @@
 
 namespace aul::tests {
 
-    template<typename T, class Alloc = std::allocator<T>>
-    bool uninitialized_default_construct_test(const Alloc& allocator = Alloc{}) {
-        constexpr std::size_t SIZE = 128;
+    TEST(Memory, default_construct) {
+        using allocator_type = std::allocator<uint32_t>;
 
-        std::unique_ptr<T[]> array{new T[SIZE]};
+        auto allocator = allocator_type{};
+        typename allocator_type::pointer allocation = std::allocator_traits<allocator_type>::allocate(allocator, 32);
 
-        aul::uninitialized_default_construct(array.get(), array.get() + SIZE, allocator);
-
-        T default_object = T{};
-        auto predicate = [&] (const T& x) -> bool {
-            return (x == default_object);
-        };
-
-        return std::all_of(array.get(), array.get() + SIZE, predicate);
+        aul::default_construct_n<typename allocator_type::pointer, int, allocator_type>(allocation, 16, allocator);
     }
 
-    template<typename T, class Alloc = std::allocator<T>>
-    bool uninitialized_default_construct_n_test(const Alloc& allocator = Alloc{}) {
-        constexpr std::size_t SIZE = 128;
-
-        std::unique_ptr<T[]> array{new T[SIZE]};
-
-        aul::uninitialized_default_construct_n(array.get(),SIZE, allocator);
-
-        T default_object = T{};
-        auto predicate = [&] (const T& x) -> bool {
-            return (x == default_object);
-        };
-
-        return std::all_of(array.get(), array.get() + SIZE, predicate);
-    }
-
-}
-
-TEST_CASE( "aul::memory::uninitialized_default_construct", "[memory]" ) {
-    CHECK(aul::tests::uninitialized_default_construct_test<char>());
-    CHECK(aul::tests::uninitialized_default_construct_test<double>());
-    CHECK(aul::tests::uninitialized_default_construct_test<std::string>());
-}
-
-TEST_CASE( "aul::memory::uninitialized_default_construct_n", "[memory]") {
-    CHECK(aul::tests::uninitialized_default_construct_n_test<char>());
-    CHECK(aul::tests::uninitialized_default_construct_n_test<double>());
-    CHECK(aul::tests::uninitialized_default_construct_n_test<std::string>());
 }
 
 #endif //AUL_MEMORY_TESTS_HPP
