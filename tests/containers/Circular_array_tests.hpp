59b49609daacc7401d00d64d243f9a88f1fc0f31
Avereniect <Avereniect@gmail.com>
2020-10-09 12:43:50 -0700

    Misc. changes

--- tests/containers/Circular_array_tests.hpp
+++ tests/containers/Circular_array_tests.hpp
@@ -5,7 +5,7 @@
 #ifndef AUL_CIRCULAR_ARRAY_TESTS_HPP
 #define AUL_CIRCULAR_ARRAY_TESTS_HPP
 
-#include "../../aul/containers/Circular_array.hpp"
+#include <aul/containers/Circular_array.hpp>
 
 #include <iostream>
 #include <gtest/gtest.h>
