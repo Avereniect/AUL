b50eea939189ed5011fdd6c134cabba8ca9febd2
Avereniect <Avereniect@gmail.com>
2021-04-10 22:00:50 -0700

    Added circular array tests to main test file

--- tests/AUL_tests.cpp
+++ tests/AUL_tests.cpp
@@ -1,4 +1,6 @@
 //#include "containers/Array_map_tests.hpp"
+//#include "containers/Circular_array_tests.hpp"
+//#include "containers/Matrix_tests.hpp"
 //#include "containers/Random_access_iterator_tests.hpp"
 //#include "containers/Slot_map_tests.hpp"
 
